c623355b99f2cee73bfad97d4710246202d130cd
Banti Shaw -X (banshaw - HCL TECHNOLOGIES LTD at Cisco) <banshaw@cisco.com>
2020-02-02 19:22:23 +0530

    Wrote login And register function

--- index.js
+++ index.js
@@ -52,38 +52,83 @@ var putOperation = function (params) {
             if (err) {
                 reject(err);
             } else {
-                resolve(data);
+                resolve({ response: "success", message: `Account created successfully` });
             }
         });
     })
 };
 
+/**
+ * Request Body
+ * {
+        "password" : "123456",
+        "phone":89613234490
+    }
+ */
 app.post('/loginUser', (req, res) => {
     let filter = {
         TableName: "loginDetails",
         Key: {
-            mobile: req.body.mobile
+            mobile: req.body.phone
         }
     }
-    return new Promise((resolve, reject) => {
-        getOperation(filter).then((result) => {
-            if (Object.keys(result).length == 0) {
-                resolve(true)
-                res.send({ response: "failure", message: `Invalid User` })
+    getOperation(filter).then((result) => {
+        if (Object.keys(result).length == 0) {
+            res.send({ response: "failure", message: `This Number does not exist` })
+        } else {
+            if (result.Item.password === req.body.password) {
+                res.send({ response: "success", message: `Welcome ${result.Item.firstName} ${result.Item.lastName}` })
             } else {
-                if (result.Item.password === req.body.password) {
-                    resolve(true)
-                    res.send({ response: "success", message: `Welcome ${result.Item.name}` })
-                } else {
-                    reject(true)
-                    res.send({ response: "failure", message: `Invalid Password` })
-                }
+                res.send({ response: "failure", message: `Invalid Password` })
             }
-        }).catch(error => {
-            console.log(chalk.red("Error occurred : ", error))
-            reject(error)
-        })
+        }
+    }).catch(error => {
+        console.log(chalk.red("Error occurred : ", error))
+        res.send({ response: "failure", message: error })
     })
+})
+
+
+/**
+ * Request Body
+ * {
+        "fname" : "Banti",
+        "sname": "Shaw",
+        "password" : "123456",
+        "email": "bantishaw9@live.com",
+        "phone":89613234490
+    }
+ */
+app.post('/register', (req, res) => {
+    let putfilter = {
+        TableName: "loginDetails",
+        Item: {
+            firstName: req.body.fname,
+            lastName: req.body.sname,
+            mobile: req.body.phone,
+            email: req.body.email,
+            password: req.body.password
+        }
+    };
+    let filter = {
+        TableName: "loginDetails",
+        Key: {
+            mobile: req.body.phone
+        }
+    }
+    getOperation(filter)
+        .then(result => {
+            if (Object.keys(result).length == 0) {
+                return putOperation(putfilter);
+            } else {
+                return { response: "success", message: `This Number already exists` }
+            }
+        }).then((response) => {
+            return res.send(response)
+        }).catch(error => {
+            console.log(chalk.red("Error in Register Operation :", error))
+            res.send({ response: "failure", message: error })
+        })
 })
 
 app.listen(8000, () => {
